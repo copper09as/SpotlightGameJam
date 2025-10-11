cf0ffeb8ccd70b664c822ebb77739d2c090a42eb
lafuluode <2506199818@qq.com>
2025-10-12 01:13:55 +0800

    完善公共mono模块

--- Assets/Script/Global/Base/MonoManager.cs
+++ Assets/Script/Global/Base/MonoManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,9 @@ using UnityEngine;
 /// </summary>
 public class MonoManager : SingleCaseMono<MonoManager>
 {
+    private Action UpdateAction;
+    private Action FixedUpdateAction;
+    private Action LateUpdateAction;
     
     // Start is called before the first frame update
     void Start()
@@ -13,9 +17,50 @@ public class MonoManager : SingleCaseMono<MonoManager>
         
     }
 
+    /// <summary>
+    /// 添加方法到公共mono模块update函数中
+    /// </summary>
+    /// <param name="action"></param>
+    public void AddUpdateAction(Action action) =>  UpdateAction += action;
+    /// <summary>
+    /// 在公共mono模块update函数中删除该方法
+    /// </summary>
+    /// <param name="action"></param>
+    public void DeleteUpdateAction(Action action) => UpdateAction -= action;
+
+    // <summary>
+    /// 添加方法到公共mono模块Fixedupdate函数中
+    /// </summary>
+    /// <param name="action"></param>
+    public void AddFixedUpdateAction(Action action) => FixedUpdateAction += action;
+
+    /// <summary>
+    /// 在公共mono模块Fixedupdate函数中删除该方法
+    /// </summary>
+    /// <param name="action"></param>
+    public void DeleteFixedUpdateAction(Action action) => FixedUpdateAction -= action;
+    // <summary>
+    /// 添加方法到公共mono模块Lateupdate函数中
+    /// </summary>
+    /// <param name="action"></param>
+    public void AddLateFixedUpdateAction(Action action) => LateUpdateAction += action;
+    /// <summary>
+    /// 在公共mono模块Lateupdate函数中删除该方法
+    /// </summary>
+    /// <param name="action"></param>
+    public void DeleteLateFixedUpdateAction(Action action) => LateUpdateAction -= action;
+
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateAction?.Invoke();
+    }
+    private void FixedUpdate()
+    {
+        FixedUpdateAction?.Invoke();
+    }
+    private void LateUpdate()
+    {
+        LateUpdateAction?.Invoke();
     }
 }
