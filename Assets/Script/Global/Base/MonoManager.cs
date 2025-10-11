21349832a7375c6d4a3266276d427ae9cd253f18
lafuluode <2506199818@qq.com>
2025-10-12 00:26:01 +0800

    公共mono模块（未完善）

---
+++ Assets/Script/Global/Base/MonoManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// ¹«¹²monoÄ£¿é
+/// </summary>
+public class MonoManager : SingleCaseMono<MonoManager>
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
