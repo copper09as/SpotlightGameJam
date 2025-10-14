423302da5b119c8017b791713e05eeaae37083c4
copper <494699676@qq.com>
2025-10-14 21:25:09 +0800

    滑块移动

--- Assets/Script/Global/Data/EntityScriptData.cs
+++ Assets/Script/Global/Data/EntityScriptData.cs
@@ -12,7 +12,6 @@ namespace Global.Data.Entity
         public int id;
         public List<string> InitPath = new List<string>();//被初始化脚本路径
         public List<string> UpdatePath = new List<string>();//Update脚本路径
-        public List<string> OnDestroyPath = new List<string>();//被摧毁脚本路径
         public List<string> OnMouseDownPath = new List<string>();//被点击脚本路径
         public List<string> OnCollisionPath = new List<string>();//在接触到时触发脚本路径
         public List<string> OnSpacePath = new List<string>();//空格按下时
