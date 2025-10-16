6ca19868735f9ea41a840e082f97a496ce8d3ff3
lafuluode <2506199818@qq.com>
2025-10-17 00:23:23 +0800

    事件中心 与 机关门开门事件（未完善）

---
+++ Assets/Script/Global/Event/EventCenter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventCenter : SingleCaseMono<EventCenter>
+{
+
+    public Dictionary<string,Action> Actions = new Dictionary<string,Action>();
+
+    public void AddAction(string name, Action action) => Actions[name] = action;
+    
+    public void RemoveAction(string name) => Actions.Remove(name); 
+
+    public void ClearActions() => Actions.Clear();
+
+    
+}
