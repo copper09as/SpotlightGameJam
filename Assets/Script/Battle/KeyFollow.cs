f1440fa47afbf3aa44ec4a3878a7ec6c7d02dc61
lafuluode <2506199818@qq.com>
2025-10-19 21:34:32 +0800

    使用dotween完成丝滑的钥匙跟随

---
+++ Assets/Script/Battle/KeyFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class KeyFollow : MonoBehaviour
+{
+    public Tween _followTweener;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
