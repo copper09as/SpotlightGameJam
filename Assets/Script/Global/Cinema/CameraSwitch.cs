9099260457147af72d735ac9e6758cdea64b0cf7
lafuluode <2506199818@qq.com>
2025-10-21 23:51:56 +0800

    相机视角切换触发器完成

--- Assets/Script/Global/Cinema/CameraSwitch.cs
+++ Assets/Script/Global/Cinema/CameraSwitch.cs
@@ -6,27 +6,19 @@ using UnityEngine;
 public class CameraSwitch : SingleCaseMono<CameraSwitch>
 {
     public CinemachineVirtualCamera[] virtualCameras;
-    private int currentCameraIndex = 0;
     // Start is called before the first frame update
-    private void Start()
+    protected override void Awake()
     {
+        base.Awake();
         virtualCameras = GetComponentsInChildren<CinemachineVirtualCamera>();
     }
-    public void Switch()//切换摄像机机位（固定两个）
+    public void Switch(CinemachineVirtualCamera virtualCamera)//切换摄像机机位（固定两个）
     {
-        currentCameraIndex++;
-        currentCameraIndex %= virtualCameras.Length;
+        
         for (int i = 0; i < virtualCameras.Length; i++)
-        virtualCameras[i].Priority = currentCameraIndex == i ? 10 : 0;
+        virtualCameras[i].Priority = virtualCamera == virtualCameras[i] ? 10 : 0;
 
         EntityUIManager.Instance.HideAllMenus();
     }
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Switch();
-        }
-    }
 }
