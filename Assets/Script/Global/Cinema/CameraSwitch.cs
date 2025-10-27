0a4eb4521ffc227b46c1264ff59fd1d638c64568
copper <494699676@qq.com>
2025-10-27 23:45:38 +0800

    相机切换

--- Assets/Script/Global/Cinema/CameraSwitch.cs
+++ Assets/Script/Global/Cinema/CameraSwitch.cs
@@ -7,35 +7,51 @@ public class CameraSwitch : MonoBehaviour
 {
     public CinemachineVirtualCamera[] virtualCameras;
     public static CameraSwitch Instance;
-    // Start is called before the first frame update
+
+    [SerializeField] private float switchRecoverDelay = 1f;
+
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
+
         virtualCameras = GetComponentsInChildren<CinemachineVirtualCamera>();
     }
+
     private void OnDestroy()
     {
-        if(Instance==this)
+        if (Instance == this)
         {
             Instance = null;
         }
     }
-    public void Switch(CinemachineVirtualCamera virtualCamera)//切换摄像机机位（固定两个）
+
+
+    public void Switch(CinemachineVirtualCamera virtualCamera)
     {
-        
         for (int i = 0; i < virtualCameras.Length; i++)
-        virtualCameras[i].Priority = virtualCamera == virtualCameras[i] ? 10 : 0;
+        {
+            virtualCameras[i].Priority = (virtualCamera == virtualCameras[i]) ? 10 : 0;
+        }
+
         EntityUIManager.Instance.isLoading = true;
         EntityUIManager.Instance.HideAllMenus();
+
+
+        StartCoroutine(RecoverLoadingFlag());
     }
 
-    public void SetTheCinameSize(float size) 
+    private IEnumerator RecoverLoadingFlag()
+    {
+        yield return new WaitForSecondsRealtime(switchRecoverDelay);
+        EntityUIManager.Instance.isLoading = false;
+    }
+
+    public void SetTheCinameSize(float size)
     {
         var virtualCamera = virtualCameras[0].Priority == 10 ? virtualCameras[0] : virtualCameras[1];
         virtualCamera.m_Lens.OrthographicSize = size;
     }
-    
 }
