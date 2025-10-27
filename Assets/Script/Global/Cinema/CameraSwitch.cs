952dc86266c792f09e3efa591c232fd26032fdc5
copper <494699676@qq.com>
2025-10-27 23:42:13 +0800

    相机switch

--- Assets/Script/Global/Cinema/CameraSwitch.cs
+++ Assets/Script/Global/Cinema/CameraSwitch.cs
@@ -3,14 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraSwitch : SingleCaseMono<CameraSwitch>
+public class CameraSwitch : MonoBehaviour
 {
     public CinemachineVirtualCamera[] virtualCameras;
+    public static CameraSwitch Instance;
     // Start is called before the first frame update
-    protected override void Awake()
+    private void Awake()
     {
-        base.Awake();
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
         virtualCameras = GetComponentsInChildren<CinemachineVirtualCamera>();
+    }
+    private void OnDestroy()
+    {
+        if(Instance==this)
+        {
+            Instance = null;
+        }
     }
     public void Switch(CinemachineVirtualCamera virtualCamera)//切换摄像机机位（固定两个）
     {
@@ -26,4 +37,5 @@ public class CameraSwitch : SingleCaseMono<CameraSwitch>
         var virtualCamera = virtualCameras[0].Priority == 10 ? virtualCameras[0] : virtualCameras[1];
         virtualCamera.m_Lens.OrthographicSize = size;
     }
+    
 }
