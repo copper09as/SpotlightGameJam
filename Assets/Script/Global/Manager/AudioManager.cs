507cf126cda013f3bcc2fef2162605b7f906fe82
Lizz76 <691561822@qq.com>
2025-10-29 13:48:03 +0800

    levels、audio

--- Assets/Script/Global/Manager/AudioManager.cs
+++ Assets/Script/Global/Manager/AudioManager.cs
@@ -30,7 +30,7 @@ public class AudioManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
         bgmSource.loop = true;
-        SetBGMVolume(0.5f);
+        SetBGMVolume(0.3f);
         SetSFXVolume(0.5f);
         AudioManager.Instance.PlaySFX("Assets/Audio/Sfx/ERROR 02.wav");
     }
